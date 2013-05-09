3950b11a0e728cf22d057c9ffbb0ecf07f67c9a1
sassunt <sassunt@gmail.com>
2013-05-09 21:02:49 +0900

    FIX content type

--- src/main/java/models/Comment.java
+++ src/main/java/models/Comment.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.nio.charset.Charset;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
@@ -13,26 +14,31 @@ import com.avaje.ebean.annotation.CreatedTimestamp;
 
 @Entity
 public class Comment {
- 
+
+	private static final Charset ENCODING = Charset.forName("UTF-8");
+
 	@Id
 	public Long id;
-	
-    public final String author;
-    
-    @CreatedTimestamp
-    public Timestamp postedAt;
-     
-    @Lob
-    public final String content;
-    
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "post_id")
-    public final Post post;
-    
-    public Comment(Post post, String author, String content) {
-        this.post = post;
-        this.author = author;
-        this.content = content;
+
+	public final String author;
+
+	@CreatedTimestamp
+	public Timestamp postedAt;
+
+	@Lob
+	public final byte[] content;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "post_id")
+	public final Post post;
+
+	public Comment(Post post, String author, String content) {
+		this.post = post;
+		this.author = author;
+		this.content = content.getBytes(ENCODING);
 	}
 
+	public String getContent() {
+		return new String(this.content, ENCODING);
+	}
 }
